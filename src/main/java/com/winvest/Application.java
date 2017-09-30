a3bc5073294be4431f65f37b15ce76c9ffbb9563
Aditya Jadhav <aditya.jadhav27@gmail.com>
2017-09-30 11:18:05 -0400

    Initial code

---
+++ src/main/java/com/winvest/Application.java
@@ -0,0 +1,19 @@
+package com.winvest;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+
+		// TODO
+
+	}
+}
